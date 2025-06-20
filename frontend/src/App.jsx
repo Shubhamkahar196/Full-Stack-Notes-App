cad268a876893d046fdea4e24903b5ace0a2f3ce
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-20 22:45:15 +0530

    Start working on full stack notes project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from "./pages/Home"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/Login"
+import  Signup from "./pages/Signup"
+
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
