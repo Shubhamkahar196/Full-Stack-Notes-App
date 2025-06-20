721900fdd6ff45e0e0bccd8772128550f9a05651
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-20 23:01:50 +0530

    Working on UI of Login

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content:  ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // color used in the project
+       colors:{
+        primary: "#2885FF",
+        secondary: "#EF863E"
+       },
+    },
   },
   plugins: [],
 }
